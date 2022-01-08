90fe53137e09db48fe7731af81f081239e3a6b1b
Maria Florencia <89933039+Florchi87@users.noreply.github.com>
2022-01-08 15:33:07 -0300

    Add files via upload

---
+++ New_Guitar.py
@@ -0,0 +1,55 @@
+def run():
+    guitar1 = "            °||||°                "
+    contador = 0
+
+
+    while contador < 4:
+        print(guitar1)
+        contador = contador + 1
+
+    guitar2 = "              ||                  "
+    contador = 0
+
+    while contador < 3:
+        print(guitar2)
+        contador = contador + 1
+
+
+    guitar3 = "            * || *                "
+    print(guitar3)
+
+
+    guitar4 = "         *    ||    *             "
+    print(guitar4)
+
+
+    guitar5 = "        *     ||     *            "
+    print(guitar5)
+
+
+    print(guitar4)
+
+    guitar6 = "          *   ||   *              "
+    print(guitar6)
+
+
+    print(guitar5)
+
+
+    guitar7 = "       *     (||)     *          "
+    print(guitar7)
+
+    guitar8 = "      *     ( || )     *         "
+    print(guitar8)
+
+    print(guitar7)
+
+    guitar9 = "        *            *          "
+    print(guitar9)
+
+    guitar10 = "          *   *   *             "
+    print(guitar10)
+
+
+if __name__ == "__main__":
+    run()
